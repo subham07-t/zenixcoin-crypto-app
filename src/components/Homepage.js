8a77d08d667cc77b049d50a6e5b1291244f00f8a
Subham07-t <subhamhaldar70@gmail.com>
2023-02-04 13:24:22 +0530

    test v5

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -51,7 +51,7 @@ const Homepage = () => {
       </Row>
       <div className="home-heading-container">
         <Title level={2} className="home-title">
-          Top 10 Cryptocurrencies in the world
+          Top 10 Cryptocurrencies in the world :
         </Title>
         <Title level={3} className="show-more">
           <Link to={"//cryptocurrencies"}>Show More</Link>
